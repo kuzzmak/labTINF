b4af2818376bd231035b610c7bc45f5e7ac80803
Antonio Kuzminski <kuzminski.antonio@gmail.com>
2019-12-25 14:17:20 +0100

    update

--- BinaryBlockCode.py
+++ BinaryBlockCode.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math
 
 
 class BinaryBlockCode(tk.Tk):
@@ -33,10 +34,19 @@ class BinaryBlockCode(tk.Tk):
         self.rows = 0
         # broj stupaca generirajuce matrice
         self.columns = 0
-
+        # lista svih generiranih polja za unos
         self.entries = []
-
+        # generirajuca matrica
         self.genMat = []
+        # abeceda koda
+        self.alphabet = [0, 1]
+        # vektorski prostor
+        self.n = 0
+        # dimenzija koda
+        self.k = 0
+        # sve kodne rijeci koda
+        self.codeWords = []
+
 
         for F in (StartPage, InstructionsPage, GenMatPage):
 
@@ -63,6 +73,7 @@ class BinaryBlockCode(tk.Tk):
             try:
                 self.rows = int(self.frames[GenMatPage].entryRows.get())
                 self.console.insert(tk.END, "[INFO] broj redaka: " + str(self.rows) + "\n")
+                self.console.see(tk.END)
             except ValueError:
                 self.console.insert(tk.END, "[ERROR] broj redaka mora biti pozitivan broj\n")
                 self.console.see(tk.END)
@@ -75,6 +86,7 @@ class BinaryBlockCode(tk.Tk):
             try:
                 self.columns = int(self.frames[GenMatPage].entryColumns.get())
                 self.console.insert(tk.END, "[INFO] broj stupaca: " + str(self.columns) + "\n")
+                self.console.see(tk.END)
             except ValueError:
                 self.console.insert(tk.END, "[ERROR] broj stupaca mora biti pozitivan broj\n")
                 self.console.see(tk.END)
@@ -85,8 +97,15 @@ class BinaryBlockCode(tk.Tk):
 
     def makeEntries(self):
 
+        self.frames[GenMatPage].buttonCodingSpeed['state'] = 'disabled'
+        self.frames[GenMatPage].buttonNormalize['state'] = 'disabled'
+        self.frames[GenMatPage].buttonLin['state'] = 'disabled'
+        self.frames[GenMatPage].buttonNK['state'] = 'disabled'
+
         for widget in self.frames[GenMatPage].entryFrame.winfo_children():
             widget.destroy()
+
+        self.entries = []
 
         for i in range(self.rows):
 
@@ -116,11 +135,214 @@ class BinaryBlockCode(tk.Tk):
                     row[j] = inputs[j + self.columns * i]
                 self.genMat.append(row)
 
+            self.frames[GenMatPage].buttonCodingSpeed['state'] = 'normal'
+            self.frames[GenMatPage].buttonNormalize['state'] = 'normal'
+            self.frames[GenMatPage].buttonLin['state'] = 'normal'
+            self.frames[GenMatPage].buttonNK['state'] = 'normal'
+
         except ValueError:
             self.console.insert(tk.END, "[ERROR] sve vrijednosti moraju biti iz skupa [0, 1]\n")
             self.console.see(tk.END)
 
+    def xor(self, row, patternRow, destinationIndex):
 
+        v1 = self.genMat[row]
+        v2 = self.genMat[patternRow]
+
+        v3 = [0] * v1.__len__()
+
+        for i in range(v3.__len__()):
+            v3[i] = v1[i] ^ v2[i]
+
+        self.genMat[destinationIndex] = v3
+
+    def isGenMatStandardized(self):
+        #FIXME
+        flag = True
+
+        for i in range(self.k):
+
+            if self.genMat[i][i] == 0:
+                print("generirajuca matrica nije standardizirana")
+                flag = False
+                break
+
+            for j in range(self.k):
+                if j == i:
+                    continue
+                if self.genMat[i][j] == 1:
+                    print("generirajuca matrica nije standardizirana")
+                    flag = False
+                    break
+        if flag:
+            print("generirajuca matrica je standardizirana")
+
+    def swapRows(self, r1, r2):
+
+        temp = self.genMat[r1]
+        self.genMat[r1] = self.genMat[r2]
+        self.genMat[r2] = temp
+
+    def swapColumns(self, c1, c2):
+
+        numOfRows = self.genMat.__len__()
+
+        for row in range(numOfRows):
+            temp = self.genMat[row][c1]
+            self.genMat[row][c1] = self.genMat[row][c2]
+            self.genMat[row][c2] = temp
+
+    def getColumn(self, index):
+
+        numOfRows = self.genMat.__len__()
+
+        result = [0] * numOfRows
+
+        for row in range(numOfRows):
+            result[row] = self.genMat[row][index]
+
+        return result
+
+    def containsColumn(self, index):
+
+        numOfRows = self.genMat.__len__()
+        numOfColumns = self.genMat[0].__len__()
+
+        # trazeni stupac koji ima na poziciji index jedinicu
+        target = [0] * numOfRows
+        target[index] = 1
+
+        for column in range(numOfColumns):
+
+            temp = self.getColumn(column)
+
+            if temp == target:
+                return column
+
+        return -1
+
+    def findPattern(self, currentColumn):
+
+        numOfRows = self.genMat.__len__()
+
+        pattern = [0] * (currentColumn + 1)
+        pattern[currentColumn] = 1
+
+        for row in range(numOfRows):
+            ok = True
+            for i in range(pattern.__len__()):
+                if self.genMat[row][i] != pattern[i]:
+                    ok = False
+                    break
+            if ok:
+                return row
+        return -1
+
+    def multiplyMod2(self, v):
+
+        numOfRows = self.genMat.__len__()
+        numOfColumns = self.genMat[0].__len__()
+
+        result = [0] * numOfColumns
+
+        for c in range(numOfColumns):
+
+            column = self.getColumn(c)
+
+            for i in range(numOfRows):
+                result[c] += v[i] * column[i]
+
+        return [x % 2 for x in result]
+
+    def generateCodeWords(self, dim):
+        """ funkcija za stvaranje kodnih rijeci dimenzije :param dim
+        :param dim: dimenzija pojedine kodne rijeci
+        :return: lista kodnih rijeci
+        """
+
+        # lista kodnih rijeci
+        codeWords = []
+
+        # kodnih rijeci ima 2 na zeljenu duljinu, odnosno dim
+        for i in range(int(math.pow(2, dim))):
+
+            tempCodeWord = [0] * dim
+
+            # pretvaranje decimalne vrijednosti u binarnu
+            binPart = bin(i).split("b")[1]
+            # dodavanje prefiksnih nula do dimenzije dim
+            while binPart.__len__() != dim:
+                binPart = '0' + binPart
+
+            for j in range(binPart.__len__()):
+                tempCodeWord[j] = int(binPart[j])
+
+            codeWords.append(tempCodeWord)
+
+        return codeWords
+
+    def normalize(self):
+
+        numOfRows = self.genMat.__len__()
+
+        for column in range(numOfRows):
+
+            for row in range(numOfRows):
+
+                # ako smo na dijagonali gdje bi elementi trebali biti 1
+                if row == column:
+
+                    if self.genMat[row][column] != 1:
+
+                        # probamo pronaci postoji li gotov stupac koji ima jedinicu
+                        # na pravom mjestu a na ostalim mjestima nule
+                        index = self.containsColumn(column)
+                        if index != -1:
+                            self.swapColumns(index, column)
+                            break
+
+                        index = self.findPattern(column)
+                        if index != -1:
+                            patternRow = index
+                            self.xor(row, patternRow, row)
+                            continue
+
+                else:  # sve ostalo kad nismo na dijagonali
+
+                    if self.genMat[row][column] == 0:
+                        continue
+                    else:
+
+                        index = self.containsColumn(column)
+                        if index != -1:
+                            self.swapColumns(index, column)
+                            break
+
+                        index = self.findPattern(column)
+                        if index != -1:
+                            patternRow = index
+                            self.xor(row, patternRow, row)
+                            continue
+
+        self.genMatPrint()
+
+    def genMatPrint(self):
+
+        self.console.insert(tk.END, "[INFO] ispis generirajuce matrice\n")
+        self.console.see(tk.END)
+
+        for row in self.genMat:
+            self.console.insert(tk.END, str(row) + "\n")
+            self.console.see(tk.END)
+
+    def showNK(self):
+        pass #TODO
+
+    def linear(self):
+        pass  #TODO
+
+    def codingSpeed(self):
+        pass #TODO
 
 
 class StartPage(tk.Frame):
@@ -182,21 +404,32 @@ class GenMatPage(tk.Frame):
         self.entryColumns = tk.Entry(inputFrame)
         self.entryColumns.pack(side="left", padx=5, pady=10)
 
+        # gumbi ----------------------------------------
         buttonFrame = tk.Frame(self)
         buttonFrame.pack()
 
-        buttonGenMat = tk.Button(buttonFrame, text="Generiraj matricu", command=lambda: [controller.saveRowsAndColumns(),
+        buttonInput = tk.Button(buttonFrame, text="unos", command=lambda: [controller.saveRowsAndColumns(),
                                                                                   controller.makeEntries()])
+        buttonInput.pack(side="left", padx=10, pady=10)
+
+        buttonGenMat = tk.Button(buttonFrame, text="generiraj matricu", command=controller.getGenMat)
         buttonGenMat.pack(side="left", padx=10, pady=10)
 
-        buttonPrintGenMat = tk.Button(buttonFrame, text="Napravi matricu", command=controller.getGenMat)
-        buttonPrintGenMat.pack(side="right", padx=10, pady=10)
+        self.buttonNK = tk.Button(buttonFrame, text="n i k zadanog koda", state="disabled", command=lambda: controller.showNK())
+        self.buttonNK.pack(side="left", padx=10, pady=10)
 
+        self.buttonLin = tk.Button(buttonFrame, text="je li linearan?", state="disabled", command=lambda: controller.linear())
+        self.buttonLin.pack(side="left", padx=10, pady=10)
+
+        self.buttonNormalize = tk.Button(buttonFrame, text="normaliziraj", state="disabled", command=lambda: controller.normalize())
+        self.buttonNormalize.pack(side="left", padx=10, pady=10)
+
+        self.buttonCodingSpeed = tk.Button(buttonFrame, text="kodna brzina", state="disabled", command=lambda: controller.codingSpeed())
+        self.buttonCodingSpeed.pack(side="left", padx=10, pady=10)
+
+        # frame s generiranim poljima za unos
         self.entryFrame = tk.Frame(self)
         self.entryFrame.pack(padx=10, pady=10)
-
-
-
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -21,151 +21,27 @@ def multiply(const, v):
         result.append(const * v[i])
     return result
 
-def xor(row, patternRow, genMat, destinationIndex):
-
-    v1 = genMat[row]
-    v2 = genMat[patternRow]
-
-    v3 = [0] * v1.__len__()
-
-    for i in range(v3.__len__()):
-        v3[i] = v1[i] ^ v2[i]
-
-    genMat[destinationIndex] = v3
-
-def isGenMatStandardized(genMat):
-
-    flag = True
-
-    for i in range(k):
-
-        if genMat[i][i] == 0:
-            print("generirajuca matrica nije standardizirana")
-            flag = False
-            break
-
-        for j in range(k):
-            if j == i:
-                continue
-            if genMat[i][j] == 1:
-                print("generirajuca matrica nije standardizirana")
-                flag = False
-                break
-    if flag:
-        print("generirajuca matrica je standardizirana")
-
-def swapRows(genMat, r1, r2):
-
-    temp = genMat[r1]
-    genMat[r1] = genMat[r2]
-    genMat[r2] = temp
-
-def swapColumns(genMat, c1, c2):
-
-    numOfRows = genMat.__len__()
-
-    for row in range(numOfRows):
-        temp = genMat[row][c1]
-        genMat[row][c1] = genMat[row][c2]
-        genMat[row][c2] = temp
-
-def getColumn(genMat, index):
-
-    numOfRows = genMat.__len__()
-
-    result = [0] * numOfRows
-
-    for row in range(numOfRows):
-        result[row] = genMat[row][index]
-
-    return result
-
-def containsColumn(genMat, index):
-
-    numOfRows = genMat.__len__()
-    numOfColumns = genMat[0].__len__()
-
-    # trazeni stupac koji ima na poziciji index jedinicu
-    target = [0] * numOfRows
-    target[index] = 1
-
-    for column in range(numOfColumns):
-
-        temp = getColumn(genMat, column)
-
-        if temp == target:
-            return column
-
-    return -1
-
-def findPattern(genMat, currentColumn):
-
-    numOfRows = genMat.__len__()
-
-    pattern = [0] * (currentColumn + 1)
-    pattern[currentColumn] = 1
-
-    for row in range(numOfRows):
-        ok = True
-        for i in range(pattern.__len__()):
-            if genMat[row][i] != pattern[i]:
-                ok = False
-                break
-        if ok:
-            return row
-    return -1
-
-def multiplyMod2(genMat, v):
-
-    numOfRows = genMat.__len__()
-    numOfColumns = genMat[0].__len__()
-
-    result = [0] * numOfColumns
-
-    for c in range(numOfColumns):
-
-        column = getColumn(genMat, c)
-
-        for i in range(numOfRows):
-
-            result[c] += v[i] * column[i]
-
-    return [x % 2 for x in result]
-
-def generateCodeWords(dim):
-    """ funkcija za stvaranje kodnih rijeci dimenzije :param dim
-    :param dim: dimenzija pojedine kodne rijeci
-    :return: lista kodnih rijeci
-    """
-
-    # lista kodnih rijeci
-    codeWords = []
-
-    # kodnih rijeci ima 2 na zeljenu duljinu, odnosno dim
-    for i in range(int(math.pow(2, dim))):
-
-        tempCodeWord = [0] * dim
-
-        # pretvaranje decimalne vrijednosti u binarnu
-        binPart = bin(i).split("b")[1]
-        # dodavanje prefiksnih nula do dimenzije dim
-        while binPart.__len__() != dim:
-            binPart = '0' + binPart
-
-        for j in range(binPart.__len__()):
-            tempCodeWord[j] = int(binPart[j])
-
-        codeWords.append(tempCodeWord)
-
-    return codeWords
 
 
 
-# print("please input your matrix\n1,2,3 -> row 1\n1,0,1 -> row 2\n.\n.\n.\n")
-# # abeceda koda
-# alphabet = [1, 0]
-# # generirajuca matrica
-# genMat = [[0, 0, 1, 1, 1], [1, 1, 0, 1, 1]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
 # code = []
 #
@@ -191,11 +67,7 @@ def generateCodeWords(dim):
 # #     print("linearan")
 #
 # isGenMatStandardized(genMat)
-#
-# # print(code)
-# print(genMat)
-# swapRows(genMat, 0, 1)
-# print(genMat)
+
 
 genMat1 = [[0, 0, 1, 1, 1],
           [1, 1, 0, 1, 1]]
@@ -209,49 +81,7 @@ genMat3 = [[1, 0, 0, 1, 1, 1, 0],
            [0, 0, 0, 1, 0, 1, 1],
            [0, 0, 1, 1, 1, 0, 0]]
 
-def normalize(genMat):
 
-    numOfRows = genMat.__len__()
-    numOfColumns = genMat[0].__len__()
-
-    for column in range(numOfRows):
-
-        for row in range(numOfRows):
-
-            # ako smo na dijagonali gdje bi elementi trebali biti 1
-            if row == column:
-
-                if genMat[row][column] != 1:
-
-                    # probamo pronaci postoji li gotov stupac koji ima jedinicu
-                    # na pravom mjestu a na ostalim mjestima nule
-                    index = containsColumn(genMat, column)
-                    if index != -1:
-                        swapColumns(genMat, index, column)
-                        break
-
-                    index = findPattern(genMat, column)
-                    if index != -1:
-                        patternRow = index
-                        xor(row, patternRow, genMat, row)
-                        continue
-
-            else: # sve ostalo kad nismo na dijagonali
-
-                if genMat[row][column] == 0:
-                    continue
-                else:
-
-                    index = containsColumn(genMat, column)
-                    if index != -1:
-                        swapColumns(genMat, index, column)
-                        break
-
-                    index = findPattern(genMat, column)
-                    if index != -1:
-                        patternRow = index
-                        xor(row, patternRow, genMat, row)
-                        continue
 
 normalize(genMat2)
 
